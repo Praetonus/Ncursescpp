5df17542292c4538344762a9cf1596fc0568f16e
Benoît Vey <benoit.vey@etu.upmc.fr>
2015-09-19 01:40:36 +0200

    Add global library header

---
+++ src/ncursescpp.hpp
@@ -0,0 +1,9 @@
+#ifndef NCURSESCPP_NCURSESCPP_HPP_
+#define NCURSESCPP_NCURSESCPP_HPP_
+
+#include "Ncurses.hpp"
+#include "Color.hpp"
+#include "constants.hpp"
+#include "errors.hpp"
+
+#endif // Header guard
